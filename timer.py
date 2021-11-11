e048ff5fd1d805db96730cb89e7501057d4bf99e
nevergreen <zhangdongyang@txzhiji.com>
2021-11-11 11:44:32 +0800

    '京东时间服务器变更'

--- timer.py
+++ timer.py
@@ -12,13 +12,16 @@ class Timer(object):
     def __init__(self, sleep_interval=0.5):
         # '2018-09-28 22:45:50.000'
         # buy_time = 2020-12-22 09:59:59.500
-        buy_time_everyday = global_config.getRaw('config', 'buy_time').__str__()
+        buy_time_everyday = global_config.getRaw(
+            'config', 'buy_time').__str__()
         localtime = time.localtime(time.time())
         self.buy_time = datetime.strptime(
-            localtime.tm_year.__str__() + '-' + localtime.tm_mon.__str__() + '-' + localtime.tm_mday.__str__()
+            localtime.tm_year.__str__() + '-' + localtime.tm_mon.__str__() +
+            '-' + localtime.tm_mday.__str__()
             + ' ' + buy_time_everyday,
             "%Y-%m-%d %H:%M:%S.%f")
-        self.buy_time_ms = int(time.mktime(self.buy_time.timetuple()) * 1000.0 + self.buy_time.microsecond / 1000)
+        self.buy_time_ms = int(time.mktime(
+            self.buy_time.timetuple()) * 1000.0 + self.buy_time.microsecond / 1000)
         self.sleep_interval = sleep_interval
 
         self.diff_time = self.local_jd_time_diff()
@@ -28,10 +31,10 @@ class Timer(object):
         从京东服务器获取时间毫秒
         :return:
         """
-        url = 'https://a.jd.com//ajax/queryServerData.html'
+        url = 'https://api.m.jd.com/client.action?functionId=queryMaterialProducts&client=wh5'
         ret = requests.get(url).text
         js = json.loads(ret)
-        return int(js["serverTime"])
+        return int(js["currentTime2"])
 
     def local_time(self):
         """
@@ -48,7 +51,8 @@ class Timer(object):
         return self.local_time() - self.jd_time()
 
     def start(self):
-        logger.info('正在等待到达设定时间:{}，检测本地时间与京东服务器时间误差为【{}】毫秒'.format(self.buy_time, self.diff_time))
+        logger.info('正在等待到达设定时间:{}，检测本地时间与京东服务器时间误差为【{}】毫秒'.format(
+            self.buy_time, self.diff_time))
         while True:
             # 本地时间减去与京东的时间差，能够将时间误差提升到0.1秒附近
             # 具体精度依赖获取京东服务器时间的网络时间损耗
